eebdb18117c9e78b37f52bd875b311a94c9220bb
Kat Kime <katckime@gmail.com>
2020-11-18 13:45:36 -0800

    Removes Keys from ParseApplication
    Removes client key and app id and replaces it with secret key strings

--- app/src/main/java/com/example/fond/ParseApplication.java
+++ app/src/main/java/com/example/fond/ParseApplication.java
@@ -5,16 +5,17 @@ import android.app.Application;
 import com.parse.Parse;
 
 public class ParseApplication extends Application {
+    String clientKey = BuildConfig.CLIENT_KEY;
+    String appId = BuildConfig.APP_ID;
 
     // Initializes Parse SDK as soon as the application is created
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // TODO: Figure out why receiving an occasional "com.parse.ParseRequest$ParseRequestException: i/o failure" error
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("BGw3ZXQSjmfO7mIwrcZ2FNNnytM8Ch0AeOM9Xnys")
-                .clientKey("iQo1J2mgMMoXcogcMQ911Zv5PzsDfiXzNzFy74HF")
+                .applicationId(appId)
+                .clientKey(clientKey)
                 .server("https://parseapi.back4app.com")
                 .build()
         );
