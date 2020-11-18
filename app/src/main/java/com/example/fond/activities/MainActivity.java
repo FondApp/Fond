4af20510023013dabf4f993c596e7a488554a554
Kat Kime <katckime@gmail.com>
2020-11-17 19:29:05 -0800

    Adding Setters and Getters to UserPost class
    - Adds code for UserPost.java
    - Registers UserPost.class on ParseApplication.java
    - Adds a tag, "MainActivity" to MainActivity.java
    - Imports ParseUser and Log to MainActivity.java

--- app/src/main/java/com/example/fond/activities/MainActivity.java
+++ app/src/main/java/com/example/fond/activities/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.fond.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.fond.R;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
