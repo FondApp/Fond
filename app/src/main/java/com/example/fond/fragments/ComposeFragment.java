9e151072351c6f951281b7c67b508f6cf8366315
Kat Kime <katckime@gmail.com>
2020-11-24 18:14:42 -0800

    Adding Compose Fragment and Class
    - Adds ComposeFragment
    - Adds fragment_compose.xml

---
+++ app/src/main/java/com/example/fond/fragments/ComposeFragment.java
@@ -0,0 +1,19 @@
+package com.example.fond.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fond.R;
+
+public class ComposeFragment extends Fragment {
+    public ComposeFragment() {
+        // Required empty public constructor
+    }
+
+
+}
