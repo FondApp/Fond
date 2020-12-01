aa2a599d47e1a48dbbc62b46f805661ebf879b20
GavinSlusher <GavinSlusher@gmail.com>
2020-12-01 10:02:46 -0500

    Search toolbar works with toast

--- app/src/main/java/com/example/fond/fragments/searchRecipeFragment.java
+++ app/src/main/java/com/example/fond/fragments/searchRecipeFragment.java
@@ -1,14 +1,20 @@
 package com.example.fond.fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.fond.R;
 
@@ -21,6 +27,7 @@ public class searchRecipeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
 //        onCreate() is called to do initial creation of the fragment (Data initialization)
+        setHasOptionsMenu(true);  // Need this so that onCreateOptionsMenu is called in the fragment
         super.onCreate(savedInstanceState);
 
     }
@@ -37,6 +44,30 @@ public class searchRecipeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 //     called after onCreateView() and ensures that the fragment's root view is non-null.
 //     Any view setup should happen here. E.g., view lookups, attaching listeners
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        MenuItem searchItem = menu.findItem(R.id.miSearch);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Filter...");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(getContext(), "You searched for " + query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 }
