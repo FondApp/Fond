b9dcdfd8482aeb695fb2d3948f7b1ce4d16673ec
Kat <katckime@gmail.com>
2020-12-01 21:28:16 -0800

    Create MyAppGlideModule.java
    Creating this class per Glide implementation in CodePath

---
+++ app/src/main/java/com/example/fond/MyAppGlideModule.java
@@ -0,0 +1,11 @@
+package com.example.fond;
+
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+
+    // leave empty for now
+}
